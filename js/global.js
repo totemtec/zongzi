72f63d6e88aa125875648991e7ac50d19573be9d
majianglin <majianglin@totemtec.com>
2020-06-19 02:29:37 +0800

    集赞完成

--- js/global.js
+++ js/global.js
@@ -11,3 +11,47 @@ function getUser() {
     }
     return null;
 }
+
+function getUserInfo(shareKey){
+
+    let url = 'https://wxspapi.totemtec.com/user/info';
+    if (shareKey) {
+        url = url + '?uk='+shareKey;
+    }
+
+    $.getJSON( url, function (res) {
+        if (res.code == 1) {
+            let user = res.data;
+            let shareUser = res.shareUser;
+            
+            setUser(user);
+
+            showInfo(user, shareUser, shareKey);
+        }
+    });
+}
+
+function likeFriend(shareKey){
+
+    if (!shareKey) return;
+
+    let url = 'https://wxspapi.totemtec.com/user/like' + '?uk='+ shareKey;
+    $.getJSON( url, function (res) {
+        if (res.code == 1) {
+            let user = res.data;
+            let shareUser = res.shareUser;
+
+            showInfo(user, shareUser, shareKey);
+        } else if (res.code > 1000) {
+            alert(res.message);
+        }
+    });
+}
+
+
+function showInfo(user, shareUser, key) {
+    console.log(user.id);
+    if (zongziPage) {
+        showInfoOnPage(user, shareUser, key);
+    }
+}
--- js/wechat.js
+++ js/wechat.js
@@ -41,32 +41,6 @@ $(function () {
         });
     }
 
-    function getUserInfo(){
-
-        let url = 'https://wxspapi.totemtec.com/user/info';
-        if (shareKey) {
-            url = url + '?uk='+shareKey;
-        }
-
-        $.getJSON( url, function (res) {
-            if (res.code == 1) {
-                let user = res.data;
-                let shareUser = res.shareUser;
-                
-                setUser(user);
-
-                showInfo(user, shareUser, shareKey);
-            }
-        });
-    }
-
-    function showInfo(user, shareUser, key) {
-        console.log(user.id);
-        if (zongziPage) {
-            showInfoOnPage(user, shareUser, key);
-        }
-    }
-
     function login(appid, code){
 
         let url = 'https://wxspapi.totemtec.com/user/login?code=' + code + '&appid=' + appid;
@@ -119,7 +93,7 @@ $(function () {
                     showInfo(user, null, shareKey);
                 }
 
-                getUserInfo();
+                getUserInfo(shareKey);
             } else {
                 //去授权
                 let url = window.location.href
